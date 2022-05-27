aa01bab3082c6cdaf4b40a545db86daa24b43f69
JarrettK <jkirck@hotmaill.com>
2022-05-27 13:27:34 -0400

    add form component

---
+++ src/components/Appointment/form.js
@@ -0,0 +1,65 @@
+
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  const reset = () => {
+    setStudent(""); 
+    setInterviewer("");
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (!interviewer) {
+      setError("Please select interviewer");
+      return;
+    }
+
+    setError("");
+    props.onSave(student, interviewer);
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
+          />
+          <section className="appointment__validation">{error}</section>
+        </form>
+        <InterviewerList
+          value={interviewer}
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
