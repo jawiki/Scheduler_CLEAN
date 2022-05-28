b6d7db674d927741be19af35152407e4108a1230
JarrettK <jkirck@hotmaill.com>
2022-05-28 15:43:01 -0400

    restructring logic

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,9 @@
-// import React from "react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Application.scss";
+import Appointment from "components/Appointment";
 import DayList from "./DayList";
-import Appointment from "./Appointment";
+import "components/Appointment";
+import "components/Application.scss";
 import useApplicationData from "hooks/useApplicationData";
 import {
   getAppointmentsForDay,
@@ -11,45 +11,25 @@ import {
   getInterviewerForDay,
 } from "helpers/selectors";
 
-
-
 export default function Application(props) {
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
-    // const { 
-    //   state, 
-    // //   setDay, 
-    // //   // bookInterview, 
-    // //   // cancelInterview 
-    // } = useApplicationData();
-
-    const [state, setState] = useState({
-      day: "Monday",
-      days: [],
-      // you may put the line below, but will have to remove/comment hardcoded appointments variable
-      appointments: {},
-    });
-    
-const dailyAppointments = [];
-
-  // const appointments = getAppointmentsForDay(state, state.day);
-
-  const setDay = day => setState({ ...state, day });
-
-  const setDays = (days) => {
-    setState((prev) => ({ ...prev, days }));
-  }
-
-  useEffect(() => {
-  axios.get("/api/days").then(response => setDays(response.data));
-    }, []);
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewerForDay(state, state.day);
 
   const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
-        interview={appointment.interview}
+        interview={interview}
+        interviewers={dailyInterviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
@@ -63,21 +43,18 @@ const dailyAppointments = [];
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
-        <DayList
-          days={state.days}
-          day={state.day}
-          // setDay={.....}
-        />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} value={state.day} onChange={setDay} />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
-        />
+        />{" "}
       </section>
       <section className="schedule">
         {schedule}
-        <Appointment key="last" time="5pm" />
+        <Appointment time="5pm" />
       </section>
     </main>
   );
